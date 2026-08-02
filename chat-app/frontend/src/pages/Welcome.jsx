3bacf9149c9034549c04de1c8b6ea3a39a71a9c4
Ankit-nehra <ankitnehra7000@gmail.com>
2026-08-02 15:59:57 +0530

    Update Welcome.jsx

--- chat-app/frontend/src/pages/Welcome.jsx
+++ chat-app/frontend/src/pages/Welcome.jsx
@@ -25,6 +25,7 @@ export default function Welcome() {
     url: "https://microservice-based-realtime-chat-app-fjwa.onrender.com/health",
   },
 ];
+  const [loading, setLoading] = useState(false);
 const wakeUpServices = async () => {
   if (loading) return;
 
